4d54a7761bb6d3abb8ff42efbb2ca38f55719a9e
yodhanabiha <yodhanabiha2@gmail.com>
2023-11-18 17:01:24 +0700

    boostrap fixed

--- src/pages/Kaos.tsx
+++ src/pages/Kaos.tsx
@@ -1,12 +1,10 @@
-import React from "react";
-import { useSelector } from "react-redux";
 import { useAppDispatch, useAppSelector } from "../app/hooks";
 import { desc, harga, title } from "../features/kaos/kaosSlice";
 import { beliBajuAsync } from "../actions/PostBajus";
 import { Button, Card } from "react-bootstrap";
 
 
-const Kaos = () =>{
+const Kaos = () => {
 
     const dispatch = useAppDispatch();
     //INI BUAT AMBIL DATA DI SLICE
@@ -16,24 +14,21 @@ const Kaos = () =>{
     // SAMPE SINI
 
     const HandleButton = () => {
-        dispatch(beliBajuAsync({title:titleKaos, desc:descKaos, harga:hargaKaos}));
+        dispatch(beliBajuAsync({ title: titleKaos, desc: descKaos, harga: hargaKaos }));
     }
 
-    return(
-        <div>
-            <Card style={{ width: '18rem', backgroundColor:'#0000' }}>
-                <Card.Body>
-                    <Card.Title>INI HALAMAN Koas</Card.Title>
-                    <Card.Text>
-                        <p><b>Produk :</b> {titleKaos} </p>
-                        <p><b>Description :</b> {descKaos} </p>
-                        <p><b>Harga :</b> {hargaKaos} </p>
-                    </Card.Text>
-                    <Button variant="primary" onClick={HandleButton}>Beli</Button>
-                </Card.Body>
-            </Card>
-        </div>
-        
+    return (
+        <Card style={{ color: '#0D0D0D' }}>
+            <Card.Body>
+                <Card.Title>INI HALAMAN Koas</Card.Title>
+                <Card.Text>
+                    <p><b>Produk :</b> {titleKaos} </p>
+                    <p><b>Description :</b> {descKaos} </p>
+                    <p><b>Harga :</b> {hargaKaos} </p>
+                </Card.Text>
+                <Button variant="primary" onClick={HandleButton}>Beli</Button>
+            </Card.Body>
+        </Card>
     )
 }
 
